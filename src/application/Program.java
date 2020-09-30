5e08970e3e38c8252ed2338537cd48058dc6c6a4
Maycon Felipe <mayconfelipe164@gmail.com>
2020-09-30 12:08:03 -0300

    Implemmenting possible moves of rook

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,9 +28,11 @@ public class Program {
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);	
 			}catch(ChessException e){
 				System.out.println(e.getMessage());
+				System.out.println("Olá");
 				sc.nextLine();
 			}catch(InputMismatchException e) {
 				System.out.println(e.getMessage());
+				System.out.println("Olá");
 				sc.nextLine();
 			}
 		}
